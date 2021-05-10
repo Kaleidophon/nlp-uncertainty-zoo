5c9ee591e09f763954a4870d57be8c1ebc4616cb
Kaleidophon <dennis.ulmer@gmx.de>
2021-05-10 18:31:16 +0200

    :construction: Track learning rate as well

--- src/model.py
+++ src/model.py
@@ -332,6 +332,9 @@ class Model(ABC):
                 summary_writer.add_scalar(
                     "Batch train loss", batch_loss, global_batch_num
                 )
+                summary_writer.add_scalar(
+                    "Batch learning rate", self.scheduler.get_lr()[0], global_batch_num
+                )
 
             epoch_loss += batch_loss.cpu().detach()
 
