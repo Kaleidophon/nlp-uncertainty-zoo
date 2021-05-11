eeffce2b0c2cdb2e4f7c06a205aff6339c3d1fed
Kaleidophon <dennis.ulmer@gmx.de>
2021-05-11 17:21:08 +0200

    :racehorse: Decrease learning rate later following Zaremba et al. (2015)

--- src/config.py
+++ src/config.py
@@ -70,7 +70,7 @@ _TRAIN_PARAMS = {
             # "early_stopping_pat": 10,
             "grad_clip": 10,
             "gamma": 0.74,  # 1 / 1.35; in the Gal implementation you divide by gamma
-            "milestones": list(range(9, 54, 1)),
+            "milestones": list(range(13, 54, 1)),
             "init_weight": 0.04,  # Hacky way to include this for replication, this prob. won't be used anywhere else
         }
     },
