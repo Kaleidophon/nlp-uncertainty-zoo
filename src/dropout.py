19e5bc131a746afacfc430c14ecc53771aa43066
Kaleidophon <dennis.ulmer@gmx.de>
2021-05-26 15:55:28 +0200

    :bug: Fix typo

--- src/dropout.py
+++ src/dropout.py
@@ -135,7 +135,7 @@ class VariationalLSTM(Model):
             Predictions.
         """
         if num_predictions is None:
-            num_predictions = self.module.num_prediction
+            num_predictions = self.module.num_predictions
 
         X = X.to(self.device)
 
