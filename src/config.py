44f6c44ab4bdcbb182948874168ea8d6ac5f0bdc
Kaleidophon <dennis.ulmer@gmx.de>
2021-06-18 15:08:12 +0200

    :bug: Use original weight decay and learning rate decay

--- src/config.py
+++ src/config.py
@@ -78,12 +78,12 @@ _TRAIN_PARAMS = {
         # Taken from  https://github.com/yaringal/BayesianRNN/blob/master/LM_code/main_new_dropout_SOTA.lua
         "variational_lstm": {
             "early_stopping": True,
-            "weight_decay": 0,  # TODO: Debug: 1e-7,
+            "weight_decay": 1e-7,
             "lr": 1,
             "num_epochs": 30,  # Changed from 55 in original
             # "early_stopping_pat": 10,
             "grad_clip": 10,
-            "gamma": 0.865,  # TODO: Debug 0.74,  # 1 / 1.35; in the Gal implementation you divide by gamma
+            "gamma": 0.74,  # 1 / 1.35; in the Gal implementation you divide by gamma
             "milestones": torch.LongTensor(range(13, 54, 1)),
             "init_weight": 0.04,  # Hacky way to include this for replication, this prob. won't be used anywhere else
         },
