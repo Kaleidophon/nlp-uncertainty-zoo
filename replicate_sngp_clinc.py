00e4ab652287c5b116945e637873b57fe835d5db
Kaleidophon <dennis.ulmer@gmx.de>
2021-07-01 10:00:28 +0200

    :construction: Reinstate SNGP-BERT, but w/o spectral normalization

--- replicate_sngp_clinc.py
+++ replicate_sngp_clinc.py
@@ -123,14 +123,12 @@ class SNGPBert(nn.Module):
         self.output_size = output_size
         self.last_layer_size = last_layer_size
 
-        # TODO: Debug: vanilla output layer
-        self.output = nn.Linear(hidden_size, output_size)
-
         # Init custom pooler without tanh activations, copy Bert parameters
         self.custom_bert_pooler = nn.Linear(hidden_size, hidden_size)
         self.custom_bert_pooler.weight = self.bert.pooler.dense.weight
         self.custom_bert_pooler.bias = self.bert.pooler.dense.bias
 
+        """
         # Spectral norm initialization
         self.spectral_norm_upper_bound = spectral_norm_upper_bound
         self.spectral_norm = SpectralNorm.apply(
@@ -140,13 +138,11 @@ class SNGPBert(nn.Module):
             dim=0,
             eps=1e-12,
         )
-
-        # TODO: Debug
         """
+
         # Misc.
         self.last_epoch = False
         self.num_predictions = num_predictions
-        """
 
     def forward(self, x: torch.LongTensor, attention_mask: torch.FloatTensor):
         """
@@ -167,15 +163,8 @@ class SNGPBert(nn.Module):
         return_dict = self.bert.forward(x, attention_mask, return_dict=True)
         cls_activations = return_dict["last_hidden_state"][:, 0, :]
         out = self.custom_bert_pooler(cls_activations)
-
-        # TODO: Debug: Vanilla output layer
-        out = self.output(out)
-
-        """
         out = self.layer_norm(out)
-
         out = self.sngp_layer(out, update_sigma_hat_inv=self.last_epoch)
-        """
 
         return out
 
@@ -208,17 +197,8 @@ class SNGPBert(nn.Module):
         return_dict = self.bert.forward(x, attention_mask, return_dict=True)
         cls_activations = return_dict["last_hidden_state"][:, 0, :]
         out = self.custom_bert_pooler(cls_activations)
-
-        # TODO: Debug: Vanilla output layer
-        import torch.nn.functional as F
-
-        out = self.output(out)
-        out = F.softmax(out, dim=-1)
-
-        """
         out = self.layer_norm(out)
         out = self.sngp_layer.predict(out, num_predictions=num_predictions)
-        """
 
         return out
 
@@ -251,19 +231,8 @@ class SNGPBert(nn.Module):
         return_dict = self.bert.forward(x, attention_mask, return_dict=True)
         cls_activations = return_dict["last_hidden_state"][:, 0, :]
         out = self.custom_bert_pooler(cls_activations)
-
-        # TODO: Debug: Just vanilla max prob
-        import torch.nn.functional as F
-
-        out = self.output(out)
-        out = F.softmax(out, dim=-1)
-        uncertainties = 1 - torch.max(out, dim=-1)[0]
-
-        """
         out = self.layer_norm(out)
-
         uncertainties = self.sngp_layer.dempster_shafer(out, num_predictions)
-        """
 
         return uncertainties
 
@@ -361,7 +330,8 @@ def run_replication(
                 optimizer.zero_grad()
 
                 # Spectral normalization
-                sngp_bert.spectral_normalization()
+                # TODO: Debug
+                # sngp_bert.spectral_normalization()
 
                 # Save training stats
                 summary_writer.add_scalar(
@@ -374,9 +344,8 @@ def run_replication(
                 )
 
             # Invert sigma matrix after the end of the last epoch
-            # TODO: Debug
-            # if epoch == EPOCHS - 1:
-            #    sngp_bert.sngp_layer.invert_sigma_hat()
+            if epoch == EPOCHS - 1:
+                sngp_bert.sngp_layer.invert_sigma_hat()
 
             # Reset dataloader
             dl = DataLoader(dataset["train"], batch_size=BATCH_SIZE)
