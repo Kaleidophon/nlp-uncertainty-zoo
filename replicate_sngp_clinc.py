002b3e1481e6c805a9dc93a742d3d143edf85f0b
Kaleidophon <dennis.ulmer@gmx.de>
2021-07-06 16:28:44 +0200

    :bug: Fix typo in SNGP-Bert

--- replicate_sngp_clinc.py
+++ replicate_sngp_clinc.py
@@ -228,7 +228,7 @@ class SNGPBert(nn.Module):
 
         return_dict = self.bert.forward(x, attention_mask, return_dict=True)
         cls_activations = return_dict["last_hidden_state"][:, 0, :]
-        out = torch.tang(self.custom_bert_pooler(cls_activations))
+        out = torch.tanh(self.custom_bert_pooler(cls_activations))
         out = self.layer_norm(out)
         uncertainties = self.sngp_layer.dempster_shafer(out, num_predictions)
 
