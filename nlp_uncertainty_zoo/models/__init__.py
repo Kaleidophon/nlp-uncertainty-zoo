bbef54725ad06ce62a5687f4d513a97ac373422d
Kaleidophon <dennis.ulmer@gmx.de>
2021-08-12 13:34:47 +0200

    :sparkles: Allow model and module import directly from package

---
+++ nlp_uncertainty_zoo/models/__init__.py
@@ -0,0 +1,25 @@
+from nlp_uncertainty_zoo.models.bayesian_lstm import BayesianLSTM, BayesianLSTMModule
+from nlp_uncertainty_zoo.models.ddu_transformer import (
+    DDUTransformer,
+    DDUTransformerModule,
+)
+from nlp_uncertainty_zoo.models.due_transformer import (
+    DUETransformer,
+    DUETransformerModule,
+)
+from nlp_uncertainty_zoo.models.lstm import LSTM, LSTMModule
+from nlp_uncertainty_zoo.models.lstm_ensemble import LSTMEnsemble, LSTMEnsembleModule
+from nlp_uncertainty_zoo.models.sngp_transformer import (
+    SNGPTransformer,
+    SNGPTransformerModule,
+)
+from nlp_uncertainty_zoo.models.st_tau_lstm import STTauLSTM, STTauLSTMModule
+from nlp_uncertainty_zoo.models.transformer import Transformer, TransformerModule
+from nlp_uncertainty_zoo.models.variational_lstm import (
+    VariationalLSTM,
+    VariationalLSTMModule,
+)
+from nlp_uncertainty_zoo.models.variational_transformer import (
+    VariationalTransformer,
+    VariationalTransformerModule,
+)
