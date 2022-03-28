bbd17e0e527ac24d7eee5d94fb0c86dd9068dd19
Kaleidophon <dennis.ulmer@gmx.de>
2022-03-28 16:04:33 +0200

    :bug: Implement missing __len__ function for samplers

--- nlp_uncertainty_zoo/utils/samplers.py
+++ nlp_uncertainty_zoo/utils/samplers.py
@@ -154,6 +154,9 @@ class Subsampler(Sampler, ABC):
     def __iter__(self):
         return iter(self.indices)
 
+    def __len__(self):
+        return len(self.indices)
+
 
 class LanguageModellingSampler(Subsampler):
     """
