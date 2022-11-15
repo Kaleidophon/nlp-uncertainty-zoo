06245c89aa2ae174801a005319254f4e1630bb04
Kaleidophon <dennis.ulmer@gmx.de>
2022-11-15 15:54:12 +0100

    :book: Copy documentation into docs root for Github pages

--- create_docs.sh
+++ create_docs.sh
@@ -5,4 +5,5 @@ cat ../README.md | sed 's/:\([a-z_]*\):/|:\1:| /g' > ./README_DOCS.md
 mv ../README.md ../README.mm
 make clean && make html
 mv ../README.mm ../README.md
+cp -r _build/html/* ./
 cd ..
