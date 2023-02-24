0046b7f0c9bee1f3b3ee5d4625e9ed01dd2a91cd
Kaleidophon <dennis.ulmer@gmx.de>
2023-02-24 17:31:08 +0000

    :construction: Bump to 0.9.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="nlp-uncertainty-zoo",
-    version="0.9.1",
+    version="0.9.2",
     author="Dennis Ulmer",
     description="PyTorch Implementation of Models used for Uncertainty Estimation in Natural Language Processing.",
     long_description=long_description,
