c98ea1e3b6aa098d0dbb5204f6c82bc9373a49fc
Kaleidophon <dennis.ulmer@gmx.de>
2023-02-27 14:51:09 +0000

    :bug: Fix annoying transformers import error

--- nlp_uncertainty_zoo/utils/data.py
+++ nlp_uncertainty_zoo/utils/data.py
@@ -17,8 +17,15 @@ from transformers import (
     PreTrainedTokenizerBase,
     DataCollatorForLanguageModeling,
 )
-from transformers.data.data_collator import _torch_collate_batch
 from transformers.tokenization_utils_base import BatchEncoding
+
+# Fix annoying import error for this function
+try:
+    from transformers.data.data_collator import _torch_collate_batch as _collate_batch
+
+except ImportError:
+    from transformers.data.data_collator import _collate_batch
+
 
 # TYPES
 # Either one set of keyword arguments per split or dictionary mapping split to split-specific keyword args
@@ -49,7 +56,7 @@ class ModifiedDataCollatorForLanguageModeling(DataCollatorForLanguageModeling):
             )
         else:
             batch = {
-                "input_ids": _torch_collate_batch(
+                "input_ids": _collate_batch(
                     examples, self.tokenizer, pad_to_multiple_of=self.pad_to_multiple_of
                 )
             }
